1c779c7a0283d8c1969a4891a77f30f654f1e008
Wire <whyyoulittle333@gmail.com>
2023-08-05 18:36:01 -0500

    Fix download-deps script

--- download-deps.sh
+++ download-deps.sh
@@ -46,7 +46,7 @@ if [[ ! -f ./toolchain/bin/arm-linux-gnueabi-gcc ]]; then
 	echo "Downloading arm toolchain"
 	mkdir -p toolchain-down
 	cd toolchain-down
-	#wget https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/arm-linux-gnueabi/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi.tar.xz
+	wget https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/arm-linux-gnueabi/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi.tar.xz
 	echo "Decompressing toolchain..."
 	tar -xf gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi.tar.xz
 	mv gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi ../toolchain
